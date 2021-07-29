5ca9bfb7328b83e3a007cc0d5e8557416e55e08d
= <=>
2021-07-29 08:57:39 +0700

    hi

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $('#slider .owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      items: 5
+  })
+});
+
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
